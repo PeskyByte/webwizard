37ced2f8d0d2a8c5221e1a188a36cc2b91a9428b
PeskyByte <hashem.aljarrah.ha@gmail.com>
2024-12-02 00:55:53 +0300

    fucking clerk now works

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
+    <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
